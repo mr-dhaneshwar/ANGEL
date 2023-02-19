abe6e73a150358dcb11f404e43393b8912047899
mr-dhaneshwar <prasaddhaneshwar09@gmail.com>
2023-02-19 22:48:39 +0530

    Update 19-02-23

--- openapi.py
+++ openapi.py
@@ -1,7 +1,7 @@
 import openai
 import time
 
-openai.api_key = "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"
+openai.api_key = "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"
 
 def angel(prompt):
     completions = openai.Completion.create(
